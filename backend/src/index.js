f59b22e9d07b2363fcae5686d044caa02b5fe3f1
Dqnial <danial_20000@mail.ru>
2026-01-08 20:57:50 +0500

    fix: replace wildcard route with regex to solve pathToRegexp error

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.use(express.static(buildPath));
 
-  app.get("*", (req, res) => {
+  app.get(/^(?!\/api).+/, (req, res) => {
     res.sendFile(path.join(buildPath, "index.html"));
   });
 }
