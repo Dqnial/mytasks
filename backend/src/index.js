46026433712f36bab5b64fe1f415392d8e8c92ac
Dqnial <danial_20000@mail.ru>
2026-01-08 20:02:29 +0500

    feat: setup monorepo and serve static for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,28 +2,42 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import taskRoutes from "./routes/task.route.js";
 
 dotenv.config();
-const app = express();
 
-const PORT = process.env.PORT;
+const app = express();
+const PORT = process.env.PORT || 5001;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:
+      process.env.NODE_ENV === "production" ? false : "http://localhost:5173",
     credentials: true,
   })
 );
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  const buildPath = path.join(__dirname, "../../frontend/dist");
+  app.use(express.static(buildPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(buildPath, "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
